44741412ac1476fc865b41a96bd0041c02355b17
Mohit Kumar <114476835+HeyBroooo@users.noreply.github.com>
2023-02-13 04:39:21 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,31 +31,3 @@ const setToZero = () => {
 
 export default App;
 
-//   const user = [
-//     { name: "pedro", age :20},
-//     { name: "mohit", age :21},
-//     { name: "rohit", age :24},
-//   ];
-
-//   return (
-//     <div className="App">
-//       {user.map((user,key) => {
-//           return <user name={user.name} age ={user.age} />;
-//       })}
-//     </div>
-//   );
-
-// }
-
-
-  // const age = 17;
-
-  // // if (age>=18){
-  // //    return <h1>Mature</h1>;
-  // // } else {
-  // return(
-  //  <div className='App'>{age >= 18 ? <h1>Mature</h1> : <h1>Imature</h1>}
-  //  </div>
-  // );
-  // }
-  
